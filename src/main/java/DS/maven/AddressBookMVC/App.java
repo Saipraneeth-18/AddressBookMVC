43b575a9d52394bc77082d89ba65818116cd95cf
Sai Praneeth <saipraneeth>
2025-08-11 14:11:12 +0530

    Version-1

---
+++ src/main/java/DS/maven/AddressBookMVC/App.java
@@ -0,0 +1,13 @@
+package DS.maven.AddressBookMVC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
